5364ca6ee9d29557591fe4a305d2baf77f8f2dff
Kt-destroyer <kevinbalseca2005@gmail.com>
2025-06-18 22:46:13 -0500

    nuevo frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { MathJaxContext } from "better-react-mathjax";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const config = {
-  loader: { load: ["input/asciimath", "input/tex"] }
-};
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <MathJaxContext config={config}>
-      <App />
-    </MathJaxContext>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
